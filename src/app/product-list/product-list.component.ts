d40689fcd5aad096c2ddc17c247c5edc08a6e29e
muuhab <mohab.barca@gmail.com>
2022-02-26 22:18:57 +0200

    Updated add to cart alert

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,7 +12,6 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   faAngleDoubleUp = faAngleDoubleUp;
   quant: string = '';
-  addedAlert:boolean=false;
 
   constructor(private productService: ProductService) {}
 
@@ -27,9 +26,6 @@ export class ProductListComponent implements OnInit {
 
   addToCart(product: { product: Product; quant: string }): void {
     this.productService.addToCart(product.product, parseInt(product.quant));
-    this.addedAlert=true
-    setTimeout(() => {
-      this.addedAlert=false
-    }, 1500);
+    alert(`${product.product.name} has been added to cart`)
   }
 }
